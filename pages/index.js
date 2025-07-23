c289859760c116be92d5818fb27c9169f6ff9726
LabHasar <brlabahasar1@gmail.com>
2025-07-23 08:52:59 -0300

    fix erros index

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "sans-serif", padding: 20 }}>
+      <h1>Beacon‑API</h1>
+      <p>Endpoint disponível em <code>/api/beacon-inventory</code></p>
+    </div>
+  );
+}
